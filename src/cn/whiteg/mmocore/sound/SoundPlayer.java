f382f6999e395578141d332d41cde60e67c77a65
MoeWhite19 <MoeWhite19@163.com>
2020-07-14 06:43:37 +0800

    音效Loop布尔值死循环改为Loop循环次数,改为-1为无限循环

---
+++ src/cn/whiteg/mmocore/sound/SoundPlayer.java
@@ -0,0 +1,154 @@
+package cn.whiteg.mmocore.sound;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.*;
+
+public class SoundPlayer implements Sound, Runnable {
+    final List<Sound> track;
+    private final Plugin plugin;
+    private final int tick;
+    Set<Location> locations = new HashSet<>(); //正在播放的位置
+    Map<Player, Location> players = new HashMap<>(); //播放的玩家列表，如果值为Null则实时获取玩家位置
+    private int loop;
+    private int progress = 0;
+    private BukkitTask task = null;
+
+    public SoundPlayer(Plugin plugin,List<Sound> track,int loop,int tick) {
+        this.plugin = plugin;
+        this.track = track;
+        this.loop = loop;
+        this.tick = tick;
+    }
+
+    public static SoundPlayer load(Plugin plugin,ConfigurationSection cs) {
+        List<?> rawList = cs.getList("track");
+        if (rawList == null) throw new IllegalArgumentException("无效配置: " + cs.getName());
+        List<Sound> list = new ArrayList<>(rawList.size());
+        for (Object str : rawList) {
+            list.add(Sound.parseSound(str));
+        }
+        return new SoundPlayer(plugin,list,cs.getInt("loop",1),cs.getInt("tick",1));
+    }
+
+    public static SoundPlayer load(ConfigurationSection cs) {
+        return load(Bukkit.getPluginManager().getPlugins()[0],cs);
+    }
+
+    @Override
+    public void playTo(Location location) {
+        if (isEmpty()) return;
+        locations.add(location);
+        start();
+    }
+
+    @Override
+    public void playTo(Player player) {
+        if (isEmpty()) return;
+        players.put(player,null);
+        start();
+    }
+
+    @Override
+    public void playTo(Player player,Location location) {
+        if (isEmpty()) return;
+        players.put(player,location);
+        start();
+    }
+
+    @Override
+    public void stopTo(Location location) {
+        locations.remove(location);
+    }
+
+    @Override
+    public void stopTo(Player player) {
+        players.remove(player);
+    }
+
+    //是否为空
+    public boolean isEmpty() {
+        return track.isEmpty() || loop == 0;
+    }
+
+    @Override
+    public void run() {
+        if (progress >= track.size()){
+            if (loop <= -1){ //无限循环
+                progress = 0;
+            } else if (loop > 0){ //循环
+                progress = 0;
+                loop--;
+            } else { //中止循环
+                cancel();
+                return;
+            }
+        }
+
+        Sound sound = track.get(progress);
+        if (!sound.isEmpty()){
+            Map<Player, Location> map = new HashMap<>(Bukkit.getOnlinePlayers().size());
+            if (!locations.isEmpty()){
+                for (Location location : locations) {
+                    for (LivingEntity entity : location.getNearbyLivingEntities(getVolume() * 8)) {
+                        if (entity instanceof Player) map.put((Player) entity,location);
+                    }
+                }
+            } else if (!this.players.isEmpty()){
+                Iterator<Map.Entry<Player, Location>> it = this.players.entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry<Player, Location> entry = it.next();
+                    if (!entry.getKey().isOnline()){
+                        it.remove();
+                    } else {
+                        map.put(entry.getKey(),entry.getValue() != null ? entry.getValue() : entry.getKey().getLocation());
+                    }
+                }
+            } else {
+                cancel();
+                return;
+            }
+            if (!map.isEmpty()){
+                map.forEach(sound::playTo);
+            }
+        }
+        progress++;
+    }
+
+    @Override
+    public float getVolume() {
+        if (track.isEmpty()) return 0F;
+        return track.get(0).getVolume();
+    }
+
+    public synchronized void cancel() {
+        if (isRun()){
+            locations.clear();
+            players.clear();
+            progress = 0;
+            Bukkit.getScheduler().cancelTask(task.getTaskId());
+            task = null;
+        }
+    }
+
+    public void start() {
+        if (!isRun()){
+            task = Bukkit.getScheduler().runTaskTimer(plugin,this,0,tick);
+        }
+    }
+
+    public synchronized boolean isRun() {
+        return task != null;
+    }
+
+    @Override
+    public SoundPlayer clone() {
+        return new SoundPlayer(plugin,track,loop,tick);
+    }
+}
